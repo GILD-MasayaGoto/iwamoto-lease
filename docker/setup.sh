8cc0f156a71b7edaccce180695be7081c74663e8
MasayaGoto <goto.masaya@opst.jp>
2019-09-04 11:50:54 +0900

    no message

--- docker/setup.sh
+++ docker/setup.sh
@@ -16,6 +16,8 @@ openssl genrsa 2048 > $SSL_API_PATH/server.key
 openssl req -new -key $SSL_API_PATH/server.key -subj "/C=JP/ST=Tokyo/L=Shibuya-ku/O=GILD,Inc./OU=System/CN=local.iwamoto-lease.jp" > $SSL_API_PATH/server.csr
 openssl x509 -in $SSL_API_PATH/server.csr -days 3650 -req -signkey $SSL_API_PATH/server.key > $SSL_API_PATH/server.crt
 chmod 400 $SSL_API_PATH/server.key
+rm -rf $SSL_API_PATH/server.csr
+
 
 API_PROJECT_PATH=~/_projects/iwamoto-lease/src/iwamoto-lease-api
 cd $API_PROJECT_PATH
@@ -25,12 +27,11 @@ php $API_PROJECT_PATH/artisan key:generate
 chmod -R 777 $API_PROJECT_PATH/storage
 chmod -R 777 $API_PROJECT_PATH/bootstrap/cache
 
-
 CLIENT_PROJECT_PATH=~/_projects/iwamoto-lease/src/iwamoto-lease-client
 cd $CLIENT_PROJECT_PATH
-composer update
 yarn install # yarnない場合、npm install に修正して
 
+cd ~/_projects/iwamoto-lease/docker
 docker-sync start
 docker-compose up -d
 
